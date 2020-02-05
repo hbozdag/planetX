5ea7c2531b73c1c648e3f822b7f3d0b11df350c9
harunbozdag <harunbozdag@gmail.com>
2020-02-04 20:18:59 -0600

    feb four

--- src/com/class11/Mars.java
+++ src/com/class11/Mars.java
@@ -1,6 +1,13 @@
 package com.class11;
 
 public class Mars {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("this is my final code");
+	}
 
   public static void main(String []args){
     
